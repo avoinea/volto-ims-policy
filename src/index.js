25ee5f6dfcf2234a76cf7fc5adbe8e8f85c7d8eb
Alin Voinea <contact@avoinea.com>
2021-04-29 15:08:49 +0300

    Moved global settings from volto-ims-theme

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 const applyConfig = (config) => {
+  // Restrict block-style to Layout only
+  config.settings.layoutOnlyBlockStyles = true;
+
+  // Disable tags on View
+  config.settings.showTags = false;
+
+  // Enable Title block
+  config.blocks.blocksConfig.title.restricted = false;
+  config.blocks.requiredBlocks = [];
+
   return config;
 };
 
