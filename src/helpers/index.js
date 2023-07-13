29652d321800d31711399370a4df7b89d7bfba8c
nileshgulia1 <nileshgulia@gmail.com>
2023-07-13 19:43:43 +0530

    eslint .

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,16 +1,18 @@
+import { isInternalURL } from "@plone/volto/helpers";
+
 export const setImageSize = (image, scales, size) => {
-    const imageScaled = isInternalURL(image)
-      ? // Backwards compat in the case that the block is storing the full server URL
-        (() => {
-          if (scales) {
-            if (size === 'h') return scales.huge;
-            if (size === 'l') return scales.large;
-            if (size === 'm') return scales.preview;
-            if (size === 's') return scales.thumb;
-            return scales.large;
-          }
-        })()
-      : { download: image, width: '100%', height: '100%' };
-  
-    return imageScaled;
-  };
+  const imageScaled = isInternalURL(image)
+    ? // Backwards compat in the case that the block is storing the full server URL
+      (() => {
+        if (scales) {
+          if (size === "h") return scales.huge;
+          if (size === "l") return scales.large;
+          if (size === "m") return scales.preview;
+          if (size === "s") return scales.thumb;
+          return scales.large;
+        }
+      })()
+    : { download: image, width: "100%", height: "100%" };
+
+  return imageScaled;
+};
